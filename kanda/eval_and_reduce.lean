9d7b9e143a2f102e774ec677420d84e1d92ead42
Sara <kanda.sanae.v5@elms.hokudai.ac.jp>
2023-12-19 19:17:52 +0900

    19.Dec 色々試しました

--- kanda/eval_and_reduce.lean
+++ kanda/eval_and_reduce.lean
@@ -1,4 +1,5 @@
 import Init.Prelude
+import Mathlib.Tactic
 namespace functionexpantion
 /-関数外延性による計算のブロック例-/
 variable (z: Nat)
@@ -22,13 +23,13 @@ def val /-(x:Nat)-/: Nat :=
 end functionexpantion
 
 namespace funcExtest
-/-Strenge!!-/
+/-Strange!!-/
 
 def f (x : Nat) := x -- + 10 --色々変えてみました
 def g (x : Nat) := 1 + x
 def h (x : Nat) := x + 1
 
-theorem feqg_Fake : f=g := sorry
+theorem feqg_Fake : f=g := sorry --sorryを消しても実はevalで0が出た。何故？
 
 def valFake : Nat :=
   Eq.recOn (motive := fun _ _ => Nat) feqg_Fake 0 -- <-この末尾の数字が反映されている？
@@ -36,14 +37,36 @@ def valFake : Nat :=
 #eval valFake --fの値を実は反映していない。
 #reduce valFake
 
+theorem geqf_Fake : g=f := sorry
+
+def reverse_vF : Nat:=
+  Eq.recOn (motive := fun _ _ => Nat) geqf_Fake 0
+
+#eval reverse_vF --順番を入れ替えたらgを反映するわけではなかった
+
 theorem g_eq_h : g=h := funext fun x =>(Nat.add_comm 1 x)
 
 def valgh /-(x:Nat)-/ := Eq.recOn (motive := fun _ _=> Nat) g_eq_h 0 --x
 
-#eval valgh --g(0),h(0)いずれも反映していない
+#eval valgh --g(0),h(0)いずれも反映していない。
+--変数でNat→Nat型をいれられるようにして、
+--(motive := fun g h=> Nat)となるようにしてみたがこれも反映していない。
 #reduce valgh
 
+variable (a : Nat)
+theorem foo : 5 ≤ a + 5 := by
+  nth_rw 1 [←Nat.add_zero 5, add_comm]
+  apply add_le_add_right
+  exact Nat.zero_le a
+
+theorem foo2 : 5+a=a+5 :=by
+  rw [add_comm]
+
 def valtes := Eq.recOn (motive := fun _ _ => Nat) (Nat.add_zero 1) 9
+--def valtes := Eq.recOn (motive := fun _ _ => Nat) (foo) 0 --なお、これにはエラーが出る。
+--x＝yの形であるかどうかはEq.recOnが判断を要求するようだ。
+--def valtes := Eq.recOn (motive := fun _ _ => Nat) (foo2 9) 0
+--命題foo2自体は真だが、x=yの形式を要求するためaの具体的数値が要求されている。
 
 #eval valtes  --どの値を反映しているのか？　fの改変結果からfでない。値よりg,hでない
 #reduce valtes
